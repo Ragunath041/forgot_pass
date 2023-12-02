6680210284566496add4e63e53c6d9a8c4fb5b50
RAGUNATH G <ragunathg00@gmail.com>
2023-12-02 11:48:49 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [count, setCount] = useState(0);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    // You can implement your authentication logic here
+    // For this example, it just checks if username and password are not empty
+    if (username === 'Ragunath' && password === 'Ragunath@2003') {
+      setLoggedIn(true);
+      alert('Logged in successfully!');
+    } else {
+      alert(`Please enter valid username and password.${count}`);
+      setCount(count+1)
+      // if(count==3){
+        
+      // }
+    }
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+    setUsername('');
+    setPassword('');
+  };
+
+  if (loggedIn) {
+    return (
+      <div>
+        <h1>Welcome, {username}!</h1>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    );
+  } else if (count>=3){
+    return<div>
+        <h1>sorry!</h1>
+        <button >password reset</button>
+      </div>
+  }
+  else {
+    return (
+      <div>
+        <h1>Login</h1>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button onClick={handleLogin}>Login</button>
+      </div>
+    );
+  }
 }
 
 export default App;
